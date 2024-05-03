6dbfec457d1c87c2103bdaed447577dfe4838fb4
estudante alura <115159173+joiceestudantes1@users.noreply.github.com>
2024-05-03 14:16:19 -0300

    05/03

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const texto=document.querySelectorAll(".aba-conteúdo");
 
 for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+    texto[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
             texto.textContents[j].classList.remove("ativo");
@@ -10,7 +10,7 @@ for(let i=0;i <botoes.length;i++){
 
         }
 
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add(ativo);
+        texto[i].classList.add("ativo");
+        texto[i].classList.add(ativo);
     }
 }
