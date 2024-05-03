abf363dae268f46d542f862df158d6d387e3f802
estudante alura <115159173+joiceestudantes1@users.noreply.github.com>
2024-05-03 14:32:02 -0300

    ggfjjjgfyjj

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const botoes = document.querySelectorAll(".botao");
-const texto=document.querySelectorAll(".aba-conteúdo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    texto[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+ botoes[i].onclick = function () {
 
-        for(let j=0;j<botoes.length;j++){
-            texto.textContents[j].classList.remove("ativo");
-            texto [j].classList.remove(ativo);
+for (let j = 0; j < botoes.length; j++) {
+ botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+                                            }
+  botoes[i].classList.add("ativo");
+ textos[i].classList.add("ativo");
+  }
+  }
 
-        }
 
-        texto[i].classList.add("ativo");
-        texto[i].classList.add(ativo);
-    }
-}
+
+
+
+
+
+
+        
+
+
+
+
+
+
